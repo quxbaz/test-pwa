2b8cfd1fb587ca8990403e8ae4ca573cb591c5ea
David Yeung <quxbaz@gmail.com>
2021-01-05 10:23:48 -0700

    Comment.

--- public/script.js
+++ public/script.js
@@ -6,7 +6,27 @@ if ('serviceWorker' in navigator) {
   })
 }
 
+function setFetchResponseText (text) {
+  document.getElementById('fetch-response').innerText = text
+}
+
+/*
+  Triggers a fetch request to a non-existing endpoint.
+
+  Normally you would get a 404 response.
+
+  However, service workers have the ability to intercept fetch events and
+  provide an arbitrary Response.
+
+  If the checkbox is disabled, the response should be a 404 error.
+
+  If the checkbox is enabled, the service worker will intercept the fetch and
+  provide a custom message.
+*/
 document.getElementById('fetch-button').addEventListener('click', async () => {
   const response = await fetch('/road/to/nowhere')
-  console.log(response)
+  // const json = response.json()
+  // if (json.status === '') {
+    // setFetchResponseText(json.status)
+  // } else
 })
